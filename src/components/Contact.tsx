e7e46ce3e3572fd3109d2c0def68e4b1337139e9
Jhon A. <120697540+Pr0mpt5@users.noreply.github.com>
2026-03-08 19:53:37 -0400

    Updated Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -55,10 +55,10 @@ export const Contact: React.FC = () => {
                   <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
                 </svg>
               </div>
-#             <div className="method-info">
-#               <div className="method-label">Phone</div>
-#               <div className="method-value">+1 (305) 555-0123</div>
-#             </div>
+              <div className="method-info">
+                <div className="method-label">Phone</div>
+                <div className="method-value">+1 (305) 555-0123</div>
+              </div>
             </div>
 
             <div className="contact-method">
